1a466052ba89260ab3cb1d98bbba5b21340b5469
sumireming <fmh_33@qq.com>
2023-03-06 17:03:32 +0800

    t

---
+++ pages/user/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="page-wrap">
+		<button type="primary" @click="login">一键登录</button>
+	</view>
+</template>
+
+<script>
+	
+	const user = uniCloud.importObject('user')
+	
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			login () {
+				uni.login({
+					complete: async (r) => {
+						if (r.errMsg === 'login:ok') {
+							let res = await user.login(r.code)
+							console.log(res)
+						} else {
+							console.error(r)
+						}
+						
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
