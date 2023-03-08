cbe8d49d3a82cce74b8bea89728b96f2ad1ecfa6
sumireming <fmh_33@qq.com>
2023-03-08 15:06:45 +0800

    t

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,30 +1,11 @@
 <template>
 	<view class="page-wrap">
 		<Login>
-			<template>123</template>
+			<template>已登录</template>
 			<template v-slot:register>
-				<button type="primary">呼起登录</button>
+				<button type="primary">登录</button>
 			</template>
 		</Login>
-		<!-- <view class="wrap">
-			<button class="avatar-wrap" 
-				open-type="chooseAvatar" 
-				@chooseavatar="onChooseavatar">
-					<image class="avatar" :src="avatar" mode="aspectFill"></image>
-			</button>
-			<view class="nickname-wrap">
-				<input type="nickname" class="input weui-input"
-					v-model="nickname"
-					placeholder="请输入昵称"
-				/>
-			</view>
-			
-			<view class="action">
-				<button type="primary" @click="login">一键登录</button>
-				
-			</view>
-		</view> -->
-		
 	</view>
 </template>
 
@@ -46,50 +27,6 @@
 </script>
 
 <style scoped lang="scss">
-	.wrap {
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	
-	button.avatar-wrap {
-		width: 160rpx;
-		height: 160rpx;
-		border-radius: 160rpx;
-		border: none;
-		background-color: $ipieces-grey-bg-color;
-		display: block;
-		position: relative;
-	}
-	
-	
-	.avatar {
-		position: absolute;
-		left: 50%;
-		top: 50%;
-		transform: translate(-50%, -50%);
-		width: 160rpx;
-		height: 160rpx;
-	}
-	
-	.nickname-wrap {
-		margin-top: 30rpx;
-	}
-	
-	.nickname-wrap input {
-		width: 200rpx;
-		font-size: 28rpx;
-		line-height: 36rpx;
-		height: 36rpx;
-		text-align: center;
-		border-bottom: 1rpx solid $ipieces-grey-bg-color;
-	}
-	
-	.action {
-		width: 100%;
-		margin-top: 90rpx;
-	}
 	
 	
 </style>
